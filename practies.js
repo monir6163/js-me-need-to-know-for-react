3845401ba11eefe38f86d310d39304afa0ca0b92
netstermonir <monirhossain6163@gmail.com>
2021-09-12 12:37:17 +0600

    practies add

---
+++ practies.js
@@ -0,0 +1,47 @@
+/* 
+1. How js code is executed in browser
+
+2. How js handles synchronous and asynchronous calls
+
+3. setTimeot , setInterval
+
+------------
+Browser Api
+------------
+1. alert , prompt , confirm
+
+2. name of the different parts of an url (url parts)
+
+3. How to change the url using the location api
+
+4. Use history api to go teo steps back
+
+5. Session storage vs local storage
+
+6. store an object in the sessionStorage and access the value object and console it as an object
+
+--------
+Debug
+--------
+1. How to find elements that has a specific class name of elements
+
+2. add a conditional breakpoint
+
+3. Console log each call to a function (do not break)
+
+4. explore dev tool official documentation . and find out how you could be ablle to time required to execute for one million time.
+
+----------
+Debugging Practice
+----------
+1. Solve the issuse tracker 
+
+2. Solve the fancy slider issuse
+
+----------
+TypeScript
+----------
+1 . difference between typescript and javascript
+
+2. when you should use typescript
+*/
